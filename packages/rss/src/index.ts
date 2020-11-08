a60acce71654df7695c171880a1b3e8ec3bb0b20
Kerem Kazan <kerem.kazan@gmail.com>
2020-11-07 19:12:02 -0800

    sending http requests

--- packages/rss/src/index.ts
+++ packages/rss/src/index.ts
@@ -1,10 +1,32 @@
 import 'reflect-metadata';
+import * as TE from 'fp-ts/lib/TaskEither';
+import * as E from 'fp-ts/lib/Either';
 
 import got from 'got';
+import { pipe } from 'fp-ts/lib/function';
 
-const f = async () => {
-  const response = await got('https://sindresorhus.com');
-  console.log(response.body);
-};
+const request = (url: string) =>
+  TE.tryCatch(
+    () => got.get(url),
+    (reason) => 'HANDLE THIS LATER', // TODO: handle this later
+  );
 
-f();
+const promise = pipe(
+  'https://www.reddit.com/.rss',
+  request,
+  TE.map((response) => {
+    if (response.statusCode !== 200) {
+      // TODO: more sophisticated status code management
+      return E.left({
+        success: false,
+        response,
+      });
+    } else {
+      return E.right(response.body);
+    }
+  }),
+)();
+promise.then((here) => {
+  // console.log({ here });
+  console.log('DONE!');
+});
