ae8be7d258f434e6c55d1e846a91ec68bdc61e0a
Kerem Kazan <kerem.kazan@gmail.com>
2020-11-11 21:32:05 -0800

    meta tag parsing done

--- packages/rss/src/utils.ts
+++ packages/rss/src/utils.ts
@@ -4,12 +4,11 @@ import * as TE from 'fp-ts/lib/TaskEither';
 import * as E from 'fp-ts/lib/Either';
 import * as O from 'fp-ts/lib/Option';
 import got from 'got';
-import { flow, pipe } from 'fp-ts/lib/function';
+import { flow } from 'fp-ts/lib/function';
 import Parser from 'rss-parser';
 import { filterMap, map } from 'fp-ts/lib/Array';
 import { sequenceS } from 'fp-ts/lib/Apply';
 import { toBaseError } from './BaseError';
-import { filter } from 'fp-ts/lib/NonEmptyArray';
 
 const parser = new Parser();
 
@@ -36,34 +35,34 @@ const parseHtml = (html: string) =>
     return cheerio.load(html);
   }, toBaseError('html parsing error'));
 
+const htmlToMetaTags = flow(
+  (root: cheerio.Root) => Object.entries(root('meta')),
+  map(([key, value]) => ({ key, value })),
+  filterMap((cur) =>
+    cur.value.attribs
+      ? O.some({ key: cur.key, attribs: cur.value.attribs })
+      : O.none,
+  ),
+  filterMap((cur) => {
+    const contentAttribute = O.fromNullable(cur.attribs['content']);
+    const propertyAttribute = O.fromNullable(cur.attribs['property']);
+    const nameAttribute = O.fromNullable(cur.attribs['name']);
+
+    const propertyPair = sequenceS(O.option)({
+      key: propertyAttribute,
+      value: contentAttribute,
+    });
+    const namePair = sequenceS(O.option)({
+      key: nameAttribute,
+      value: contentAttribute,
+    });
+    return O.alt(() => namePair)(propertyPair);
+  }),
+);
+
 export const parseMetaTags = flow(
   parseHtml,
-  E.map(
-    flow(
-      (root) => Object.entries(root('meta')),
-      map(([key, value]) => ({ key, value })),
-      filterMap((cur) =>
-        cur.value.attribs
-          ? O.some({ key: cur.key, attribs: cur.value.attribs })
-          : O.none,
-      ),
-      filterMap((cur) => {
-        const contentAttribute = O.fromNullable(cur.attribs['content']);
-        const propertyAttribute = O.fromNullable(cur.attribs['property']);
-        const nameAttribute = O.fromNullable(cur.attribs['name']);
-
-        const propertyPair = sequenceS(O.option)({
-          key: propertyAttribute,
-          value: contentAttribute,
-        });
-        const namePair = sequenceS(O.option)({
-          key: nameAttribute,
-          value: contentAttribute,
-        });
-        return O.alt(() => namePair)(propertyPair);
-      }),
-    ),
-  ),
+  E.map(htmlToMetaTags),
   E.map((allMetaAttributes) => {
     //
   }),
