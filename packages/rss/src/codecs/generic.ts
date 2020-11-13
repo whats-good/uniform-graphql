c289481385ba4e551330fc891c90db61502a0a01
Kerem Kazan <kerem.kazan@gmail.com>
2020-11-12 19:41:19 -0800

    generic rss items

--- packages/rss/src/codecs/generic.ts
+++ packages/rss/src/codecs/generic.ts
@@ -1,13 +1,16 @@
 import * as t from 'io-ts';
 import { decode } from '../utils';
 
+const genericFeedItemCodec = t.type({
+  link: t.string,
+});
+
 const genericCodec = t.type({
   // link: t.string,
-  items: t.array(
-    t.type({
-      link: t.string,
-    }),
-  ),
+  items: t.array(genericFeedItemCodec),
 });
+
+export type GenericRssFeed = t.TypeOf<typeof genericCodec>;
+export type GenericRssFeedItem = t.TypeOf<typeof genericFeedItemCodec>;
 
 export const decodeGeneric = decode(genericCodec);
