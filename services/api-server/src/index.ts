878fe528064b9c41bcea02f61bdd9df64aec18a1
Kerem Kazan <kerem.kazan@gmail.com>
2020-11-13 11:46:41 -0800

    dropping the manual graphql typedefs

--- services/api-server/src/index.ts
+++ services/api-server/src/index.ts
@@ -2,28 +2,52 @@ import { ApolloServer, gql } from 'apollo-server-express';
 import { GraphQLJSON } from 'graphql-type-json';
 import express from 'express';
 import { data } from './cached/data';
-// import { GraphQLObjectType } from 'graphql';
+import * as t from 'io-ts';
+import {
+  GraphQLID,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from 'graphql';
 
-const typeDefs = gql`
-  type Query {
-    "A simgle type for getting started!"
-    hello: String!
-    data: JSON!
-  }
-  scalar JSON
-`;
+const codec = t.type({
+  link: t.string,
+  title: t.string,
+});
 
-const resolvers = {
-  Query: {
-    hello: () => 'world',
-    data: () => data,
+const codecObject = new GraphQLObjectType({
+  name: 'my object!',
+  fields: () => ({
+    link: { type: GraphQLString },
+    title: { type: GraphQLString },
+  }),
+});
+
+const rootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    hello: {
+      type: GraphQLString,
+      args: {
+        id: {
+          type: GraphQLID,
+        },
+      },
+      // TODO: how can we get id from above?
+      // TODO: and how can we get the return type from above?
+      resolve: (parent, args) => {
+        return 'YO';
+      },
+    },
   },
-  JSON: GraphQLJSON,
-};
+});
+
+const schema = new GraphQLSchema({
+  query: rootQuery,
+});
 
 const apolloServer = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema,
 });
 
 const PORT = 4001;
