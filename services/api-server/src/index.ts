c233cfc9d85e8ba18c71ed725c9a41c920519b88
Kerem Kazan <kerem.kazan@gmail.com>
2020-11-13 11:22:48 -0800

    graphql-server initial setup done

--- services/api-server/src/index.ts
+++ services/api-server/src/index.ts
@@ -1,5 +1,34 @@
-const f = () => {
-  const x = 'a';
+import { ApolloServer, gql } from 'apollo-server-express';
+import express from 'express';
+
+const typeDefs = gql`
+  type Query {
+    "A simgle type for getting started!"
+    hello: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'world',
+  },
 };
 
-f();
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const PORT = 4001;
+
+const start = () => {
+  const app = express();
+  apolloServer.applyMiddleware({ app });
+  app.listen({ port: PORT }, () => {
+    console.log(
+      `🚀 Server ready at http://localhost:${PORT}${apolloServer.graphqlPath}`,
+    );
+  });
+};
+
+start();
