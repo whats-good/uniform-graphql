5fd7bf1a607f4e5e7f11f16254abbd91ac83bc9e
Kerem Kazan <kerem.kazan@gmail.com>
2020-12-24 15:12:50 -0800

    created a datetime scalar

--- src/sixth-attempt/index.ts
+++ src/sixth-attempt/index.ts
@@ -3,11 +3,13 @@ import {
   GraphQLFloat,
   GraphQLID,
   GraphQLInt,
+  GraphQLNonNull,
   GraphQLObjectType,
   GraphQLScalarType,
   GraphQLSchema,
   GraphQLString,
   GraphQLType,
+  Kind,
   ValueNode,
 } from 'graphql';
 import { Type } from '../Type';
@@ -16,7 +18,7 @@ import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
 
 export class TypeContext {
-  __k?: string;
+  public readonly savedGraphQLTypes = new Map<string, GraphQLType>();
 }
 
 export abstract class SemiType<N extends string, T, R = T> {
@@ -28,16 +30,30 @@ export abstract class SemiType<N extends string, T, R = T> {
     this.name = params.name;
   }
 
-  public abstract getSemiGraphQLType(typeContext: TypeContext): GraphQLType;
+  protected abstract getFreshSemiGraphQLType(
+    typeContext: TypeContext,
+  ): GraphQLType;
+
+  // TODO: handle recursive types to avoid infinite loops
+  public getSemiGraphQLType(typeContext: TypeContext): GraphQLType {
+    const existingType = typeContext.savedGraphQLTypes.get(this.name);
+    if (existingType) {
+      return existingType;
+    } else {
+      const newType = this.getFreshSemiGraphQLType(typeContext);
+      typeContext.savedGraphQLTypes.set(this.name, newType);
+      return newType;
+    }
+  }
 }
 
 export type Maybe<T> = T | null | undefined;
 
-type ScalarSerializer<TInternal> = (value: TInternal) => Maybe<JSON>;
+type ScalarSerializer<TInternal> = (value: TInternal) => Maybe<any>;
 type ScalarValueParser<TInternal> = (value: unknown) => Maybe<TInternal>;
 type ScalarLiteralParser<TInternal> = (
   valueNode: ValueNode,
-  variables: Maybe<{ [key: string]: any }>,
+  variables: Maybe<{ [key: string]: any }>, // TODO: try a better type for serializers
 ) => Maybe<TInternal>;
 
 // serialize: GraphQLScalarSerializer<any>;
@@ -70,7 +86,7 @@ export class ScalarSemiType<N extends string, T> extends SemiType<N, T, T> {
     this.literalParser = params.parseLiteral;
   }
 
-  getSemiGraphQLType = (): GraphQLScalarType => {
+  protected getFreshSemiGraphQLType = (): GraphQLScalarType => {
     return new GraphQLScalarType({
       name: this.name,
       description: this.description,
@@ -82,7 +98,7 @@ export class ScalarSemiType<N extends string, T> extends SemiType<N, T, T> {
   };
 }
 
-export const string = new ScalarSemiType<'String', string>({
+const string = new ScalarSemiType<'String', string>({
   name: 'String',
   parseLiteral: GraphQLString.parseLiteral,
   parseValue: GraphQLString.parseValue,
@@ -91,7 +107,7 @@ export const string = new ScalarSemiType<'String', string>({
   specifiedByUrl: GraphQLString.specifiedByUrl,
 });
 
-export const int = new ScalarSemiType<'Int', number>({
+const int = new ScalarSemiType<'Int', number>({
   name: 'Int',
   parseLiteral: GraphQLInt.parseLiteral,
   parseValue: GraphQLInt.parseValue,
@@ -100,7 +116,7 @@ export const int = new ScalarSemiType<'Int', number>({
   specifiedByUrl: GraphQLInt.specifiedByUrl,
 });
 
-export const boolean = new ScalarSemiType<'Boolean', boolean>({
+const boolean = new ScalarSemiType<'Boolean', boolean>({
   name: 'Boolean',
   parseLiteral: GraphQLBoolean.parseLiteral,
   parseValue: GraphQLBoolean.parseValue,
@@ -109,7 +125,7 @@ export const boolean = new ScalarSemiType<'Boolean', boolean>({
   specifiedByUrl: GraphQLBoolean.specifiedByUrl,
 });
 
-export const float = new ScalarSemiType<'Float', number>({
+const float = new ScalarSemiType<'Float', number>({
   name: 'Float',
   parseLiteral: GraphQLFloat.parseLiteral,
   parseValue: GraphQLFloat.parseValue,
@@ -118,13 +134,33 @@ export const float = new ScalarSemiType<'Float', number>({
   specifiedByUrl: GraphQLFloat.specifiedByUrl,
 });
 
-export const id = new ScalarSemiType<'ID', number>({
+const id = new ScalarSemiType<'ID', number>({
   name: 'ID',
   parseLiteral: GraphQLID.parseLiteral,
   parseValue: GraphQLID.parseValue,
   serialize: GraphQLID.serialize,
   description: GraphQLID.description,
   specifiedByUrl: GraphQLID.specifiedByUrl,
+});
+
+export const datetime = new ScalarSemiType<'Datetime', Date>({
+  name: 'Datetime',
+  parseLiteral: (ast) => {
+    if (ast.kind === Kind.STRING) {
+      return new Date(Date.parse(ast.value));
+    }
+    return null;
+  },
+  parseValue: (value) => {
+    if (typeof value === 'string') {
+      return new Date(value);
+    }
+    return null;
+  },
+  serialize: (value) => {
+    return value.toISOString();
+  },
+  description: 'A datetime wrapper for the Date class',
 });
 
 const typeContext = new TypeContext();
@@ -134,7 +170,26 @@ const schema = new GraphQLSchema({
     name: 'RootQuery',
     fields: {
       id: {
-        type: id.getSemiGraphQLType(),
+        type: id.getSemiGraphQLType(typeContext) as any,
+      },
+      datetime: {
+        type: datetime.getSemiGraphQLType(typeContext) as any,
+        resolve: (a, b, c) => {
+          return new Date();
+        },
+      },
+      dateAsInput: {
+        type: new GraphQLNonNull(
+          datetime.getSemiGraphQLType(typeContext) as any,
+        ),
+        args: {
+          x: {
+            type: datetime.getSemiGraphQLType(typeContext) as any,
+          },
+        },
+        resolve: (a, b, c) => {
+          return b.x;
+        },
       },
     },
   }),
@@ -149,6 +204,8 @@ const PORT = 4001;
 const start = () => {
   const app = express();
   apolloServer.applyMiddleware({ app });
+
+  // const url = id.getFreshSemiGraphQLType().specifiedByUrl;
   app.listen({ port: PORT }, () => {
     console.log(
       `🚀 Server ready at http://localhost:${PORT}${apolloServer.graphqlPath}`,
