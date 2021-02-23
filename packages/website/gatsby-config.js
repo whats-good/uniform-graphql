4a72a662587fa462fe807e61e724d28d3db053ad
Kerem Kazan <kerem.kazan@gmail.com>
2021-02-23 08:58:02 -0800

    cache-busting

--- packages/website/gatsby-config.js
+++ packages/website/gatsby-config.js
@@ -25,17 +25,6 @@ module.exports = {
         baseDir: `packages/website`,
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: SITE_LONG_NAME,
-        short_name: SITE_SHORT_NAME,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        display: `standalone`,
-        icon: `static/favicon.png`,
-      },
-    },
     `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-google-analytics`,
@@ -50,6 +39,5 @@ module.exports = {
         siteUrl: SITE_URL,
       },
     },
-    `gatsby-plugin-offline`,
   ],
 };
