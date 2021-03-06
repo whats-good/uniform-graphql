d948dc4423181d8d9f512c218af55c33b51541cf
Kerem Kazan <kerem.kazan@gmail.com>
2021-03-06 15:43:26 -0800

    header fixed

---
+++ packages/apollo-docs/src/gatsby-theme-apollo-docs/components/header.js
@@ -0,0 +1,53 @@
+import React from 'react';
+// TODO: delete this file when algolia is setup.
+
+// TODO: fix apollo icon on mobile nav
+
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+
+const sizes = {
+  sm: 600,
+  md: 850,
+  lg: 1120,
+};
+
+const breakpoints = Object.keys(sizes).reduce(
+  (acc, key) => ({
+    ...acc,
+    [key]: `@media (max-width: ${sizes[key]}px)`,
+  }),
+  {},
+);
+
+const Wrapper = styled.header({
+  position: 'sticky',
+  top: 0,
+  left: 0,
+  zIndex: 1,
+});
+
+const InnerWrapper = styled.div({
+  display: 'flex',
+  alignItems: 'center',
+  // height: HEADER_HEIGHT,
+  padding: '0 56px',
+  backgroundColor: 'white',
+  [breakpoints.md]: {
+    padding: '0 24px',
+  },
+});
+
+export default function Header(props) {
+  return (
+    <Wrapper>
+      {props.beforeContent}
+      <InnerWrapper>{props.children}</InnerWrapper>
+    </Wrapper>
+  );
+}
+
+Header.propTypes = {
+  beforeContent: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
+};
